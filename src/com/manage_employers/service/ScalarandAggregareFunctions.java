074d7e95a2b856f0c89d1b16190dff8056bb5a28
alibi <mourad.alibi@enis.tn>
2018-09-16 17:28:25 +0100

    add Scalarand Aggregare Functions

--- src/com/manage_employers/service/ScalarandAggregareFunctions.java
+++ src/com/manage_employers/service/ScalarandAggregareFunctions.java
@@ -1,11 +1,32 @@
 package com.manage_employers.service;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+
 
 public class ScalarandAggregareFunctions {
 
 	public static void main(String [] args) {
-		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory(persistenceUnitName)
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("Eclipselink_JPA");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		
+		//Scalar function
+		Query query = entityManager.
+				createQuery("Select UPPER(e.ename) from Employee e");
+		List<String> list = query.getResultList();
+		for(String e:list) {
+			System.out.println("Employee NAME :"+e);
+		}
+		
+		//Aggregate Function
+		Query query1 = entityManager.createQuery("Select MAX(e.salary) from Employee e");
+		Double result = (Double) query1.getSingleResult();
+		System.out.println("Max Employee Salary :"+result);
+		
 	}
 }
