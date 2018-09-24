5cd06657bf881ed537198656b39e85dd3a6b3792
alibi <mourad.alibi@enis.tn>
2018-09-24 18:29:09 +0100

    Staff

---
+++ src/com/manage_employers/Entity/Staff.java
@@ -0,0 +1,53 @@
+package com.manage_employers.Entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+
+@Entity
+@Table
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+
+public class Staff implements Serializable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int sid;
+	private String sname;
+	
+	public Staff() {
+		super();
+	}
+
+	public Staff(int sid, String sname) {
+		super();
+		this.sid = sid;
+		this.sname = sname;
+	}
+
+	public int getSid() {
+		return sid;
+	}
+
+	public void setSid(int sid) {
+		this.sid = sid;
+	}
+
+	public String getSname() {
+		return sname;
+	}
+
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	
+	
+	
+	
+	
+}
