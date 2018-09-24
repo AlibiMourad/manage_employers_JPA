b25591a830457aa1fcd0015603f95642e619afa0
alibi <mourad.alibi@enis.tn>
2018-09-24 16:24:12 +0100

    Ordering

--- src/com/manage_employers/Entity/Employee.java
+++ src/com/manage_employers/Entity/Employee.java
@@ -4,10 +4,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
 @Table
+@NamedQuery(query = "Select e from Employee e where e.eid = :id", name= "find employee by id")
+
 public class Employee {
 
 	@Id
---
+++ src/com/manage_employers/service/Ordering.java
@@ -0,0 +1,29 @@
+package com.manage_employers.service;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+import com.manage_employers.Entity.Employee;
+
+public class Ordering {
+
+	public static void main(String [] args) {
+		
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("Eclipselink_JPA");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		
+		// Order By
+		Query query = entityManager.createQuery("select e from Employee e ORDER BY e.ename ASC" );
+		
+		List<Employee> employees = query.getResultList();
+		for(Employee e:employees) {
+			 System.out.print("Employee ID :" + e.getEid( ));
+	         System.out.println("\t Employee Name :" + e.getEname( ));
+		}
+	}
+	
+}
