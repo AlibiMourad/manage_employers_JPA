0933ca036f741e7bf0e1e4b628fd562f279260ac
alibi <mourad.alibi@enis.tn>
2018-09-24 18:33:53 +0100

    Staff2

--- src/com/manage_employers/Entity/Staff.java
+++ src/com/manage_employers/Entity/Staff.java
@@ -2,6 +2,7 @@ package com.manage_employers.Entity;
 
 import java.io.Serializable;
 
+import javax.persistence.DiscriminatorColumn;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -13,6 +14,7 @@ import javax.persistence.Table;
 @Entity
 @Table
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn( name="type")
 
 public class Staff implements Serializable {
 	@Id
